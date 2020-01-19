001c23c59db585d94046623e3c66fc118e0b1ab4
AmandaRP <amanda@dekleineorchards.com>
2020-01-19 02:18:55 +0000

    Saturday progress

--- wildFires.R
+++ wildFires.R
@@ -65,9 +65,118 @@ fires %>%
 #TODO: Recreate https://twitter.com/thomas_mock/status/1214255918533292037/photo/3
 
 library(lubridate)
-temperature %>% 
+temp_avgs <- temperature %>% 
   mutate(year = year(date)) %>%
+  drop_na() %>%
   group_by(year) %>%
-  summarize(avg_temp = mean(temperature, na.rm = TRUE), cout = n())
+  summarize(avg_temp = mean(temperature, na.rm = TRUE), count = n()) 
 
+# 2019 seems to be incomplete, so we should not use. Visualize:
+temp_avgs %>%
+  ggplot(aes(year, count)) + 
+  geom_point() 
+
+# Filter out years with fewer data points
+library(magrittr)
+temp_avgs %<>% filter(year >= 1950, year < 2019)
+
+overall_avg_temp <- temp_avgs %>% 
+  summarize(overall_avg_temp = mean(avg_temp)) %>% 
+  unlist()
+
+library('latex2exp')
+
+temp_avgs %>% 
+  mutate(temp_minus_mean = avg_temp - overall_avg_temp) %>%
+  ggplot(aes(year, temp_minus_mean, fill = temp_minus_mean<0)) + 
+  geom_col() +
+  labs(x = element_blank(), 
+       y = element_blank(),
+       title = "Australia is getting warmer",
+       caption = "Source: Australian Government Bureau of Meteorology") +
+  theme_minimal() +
+  scale_y_continuous(breaks = c(-1, -0.5, 0.5, 1),
+                   labels = c(parse(text = TeX('$-1.0^o$')), 
+                              parse(text = TeX('$-0.5^o$')), 
+                              parse(text = TeX('$+0.5^o$')), 
+                              parse(text = TeX('$+1.0^o$')))) +
+  theme(#axis.text.y = element_blank(),
+        plot.background = element_blank(),
+        panel.grid.minor.y = element_blank(),
+        panel.grid.major.y = element_blank(),
+        panel.grid.major.x = element_line(linetype = "dashed", color = "grey"),
+        panel.grid.minor.x = element_blank(),
+        plot.title = element_text(face = "bold"),
+        legend.position = "none",
+        plot.caption = element_text(color = "darkgrey")) +
+  annotate("text", 
+           x = 1962, 
+           y = 0.7, 
+           color = "darkgrey",
+           label = "Annual temperature (degrees Celcius)\nabove or below the 1950-2018 average") 
+
+
+## rainfall
+
+rainfall %>% 
+  summarize(m = min(year), max = max(year), cnt = n())
+
+
+#look at timeline by city. TODO: Need to impute missing data.
+rainfall %>%
+  filter(year >= 1950, year < 2020) %>%
+  replace_na(list(rainfall = 0)) %>%
+  #select(city_name) %>%
+  group_by(city_name, year) %>%
+  summarize(avg = mean(rainfall), count = n()) %>%
+  #distinct()
+  ggplot(aes(year,avg, group = city_name, color = city_name)) +
+  geom_line()
+  
+
+
+rain_avgs <- rainfall %>% 
+  drop_na() %>%
+  group_by(year) %>%
+  summarize(avg_rain = mean(rainfall, na.rm = TRUE), count = n()) 
+
+rain_avgs %>%
+  filter(year >= 1950, year < 2020) %>%
+  ggplot(aes(year, count)) +
+  geom_point()
+
+rain_avgs %<>% filter(year >= 1950, year < 2020)
+
+overall_avg_rain <- rain_avgs %>% 
+  summarize(overall_avg_rain = mean(avg_rain)) %>% 
+  unlist()
+
+rain_avgs %>% 
+  mutate(rain_minus_mean = avg_rain - overall_avg_rain) %>%
+  ggplot(aes(year, rain_minus_mean, fill = rain_minus_mean>0)) + 
+  geom_col() +
+  labs(x = element_blank(), 
+       y = element_blank(),
+       title = "Australia is getting dryer?",
+       caption = "Source: Australian Government Bureau of Meteorology") +
+  theme_minimal() +
+  #scale_y_continuous(breaks = c(-1, -0.5, 0.5, 1),
+  #                   labels = c(parse(text = TeX('$-1.0^o$')), 
+  #                              parse(text = TeX('$-0.5^o$')), 
+  #                              parse(text = TeX('$+0.5^o$')), 
+  #                              parse(text = TeX('$+1.0^o$')))) +
+  theme(#axis.text.y = element_blank(),
+    plot.background = element_blank(),
+    panel.grid.minor.y = element_blank(),
+    panel.grid.major.y = element_blank(),
+    panel.grid.major.x = element_line(linetype = "dashed", color = "grey"),
+    panel.grid.minor.x = element_blank(),
+    plot.title = element_text(face = "bold"),
+    legend.position = "none",
+    plot.caption = element_text(color = "darkgrey")) #+
+  #annotate("text", 
+  #         x = 1962, 
+  #         y = 0.7, 
+  #         color = "darkgrey",
+  #         label = "Annual temperature (degrees Celcius)\nabove or below the 1950-2018 average") 
 
